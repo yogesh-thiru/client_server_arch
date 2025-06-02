b78a1d1fa01e4d696df37e78f1d9a60d773f4929
yogesh-thiru <115695148+yogesh-thiru@users.noreply.github.com>
2025-06-02 13:02:30 +0530

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,47 +1,33 @@
 /******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
+
 #include <iostream>
-#include<unistd.h>
 #include<arpa/inet.h>
+#include<unistd.h>
 #include<cstring>
 using namespace std;
-
 int main()
 {
-   int serversoc = socket(AF_INET,SOCK_STREAM,0);
+   int sock = socket(AF_INET, SOCK_STREAM,0);
    
-   sockaddr_in servadd {};
-   servadd.sin_family = AF_INET;
-   servadd.sin_port = htons(54000);
-   servadd.sin_addr.s_addr = INADDR_ANY;
+   sockaddr_in servsoc{};
+   servsoc.sin_family = AF_INET;
+   servsoc.sin_port = htons(54000);
+   inet_pton(AF_INET,"127.0.0.1",&servsoc.sin_addr);
    
-   if(bind(serversoc, (sockaddr*)&servadd ,sizeof(servadd)) < 0){
-       cout<<"not bined"<<endl;
-       return 1;
-   }
-   if(listen(serversoc,1) <0){
-       cout<<"not listened"<<endl;
-   return 1;
-       
-   }
-   cout<<"waiting for client"<<endl;
-   int cli = accept(serversoc,nullptr,nullptr);
+   connect(sock,(sockaddr*)&servsoc,sizeof(servsoc));
    cout<<"connected"<<endl;
-   
-   char buffer[256]={0};
    while(true){
-       memset(buffer,0,sizeof(buffer));
-       int byterec = recv(cli,buffer,sizeof(buffer),0);
-       if(byterec < 0)break;
-       cout<<buffer<<endl;
+       string s;
+       getline(cin,s);
+       if(s == "exit")break;
+       send(sock,s.c_str(),s.size(),0);
    }
-   close(cli);
-   close(serversoc);
-   return 0;
+close(sock);
+    return 0;
 }
